23365030ed87ca2bb64abb60ecdc09eb3eb5f700
rangulo1972 <rangulo1972@gmail.com>
2023-01-31 21:23:13 -0500

    retoques finales para el example3.dart

--- lib/pages/example3_page.dart
+++ lib/pages/example3_page.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:codigo6_ui/widgets/circle_widget.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Example3Page extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     //! para tener referencia respecto a al altura de la pantalla de presentación
     double height = MediaQuery.of(context).size.height;
+    double width = MediaQuery.of(context).size.width;
     return Scaffold(
       body: Stack(children: [
         //* inicio de fondo
@@ -44,7 +46,157 @@ class Example3Page extends StatelessWidget {
         ),
         //* fin de fondo
         //* inicio formulario
-        Text("Hola mundo"),
+        SafeArea(
+            child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(26.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  height: height * 0.20,
+                ),
+                Text(
+                  "reisup",
+                  style: GoogleFonts.montserrat(
+                    color: Color(0xff3EA2A8),
+                    fontSize: 30,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+                Text(
+                  "you have goals.",
+                  style: GoogleFonts.montserrat(
+                    color: Color(0xff2E303E),
+                    fontSize: 30,
+                    fontWeight: FontWeight.w400,
+                    letterSpacing: -0.9,
+                  ),
+                ),
+                Text(
+                  "Invest to achive them.",
+                  style: GoogleFonts.montserrat(
+                    color: Color(0xff2E303E),
+                    fontSize: 30,
+                    fontWeight: FontWeight.w400,
+                    letterSpacing: -0.9,
+                  ),
+                ),
+                SizedBox(
+                  height: height * .03,
+                ),
+                TextField(
+                  cursorColor: Color(0xff3EA2A8),
+                  decoration: InputDecoration(
+                    hintText: "Email*",
+                    hintStyle: GoogleFonts.montserrat(color: Colors.black38),
+                    enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Color(0xff3ea2a8).withOpacity(0.50),
+                        width: 2.5,
+                      ),
+                    ),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Color(0xff3ea2a8).withOpacity(0.50),
+                        width: 2.5,
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: height * .04,
+                ),
+                TextField(
+                  cursorColor: Color(0xff3EA2A8),
+                  decoration: InputDecoration(
+                    hintText: "Password",
+                    hintStyle: GoogleFonts.montserrat(color: Colors.black38),
+                    enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Color(0xff3ea2a8).withOpacity(0.50),
+                        width: 2.5,
+                      ),
+                    ),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Color(0xff3ea2a8).withOpacity(0.50),
+                        width: 2.5,
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: height * 0.06,
+                ),
+                SizedBox(
+                  width: double.infinity,
+                  height: 52.0,
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    child: Text("Log in"),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Color(0xff2e303e),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(14),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: height * 0.04,
+                ),
+                //* inicio de button Sign up
+                SizedBox(
+                  width: double.infinity,
+                  height: 52.0,
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    child: Text("Sign up"),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Color(0xff3EA2A8),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(14),
+                      ),
+                    ),
+                  ),
+                ),
+                //* fin de button Sign up
+                SizedBox(
+                  height: height * .04,
+                ),
+                //* inicio de texto debajo del button Sign up
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Forgot username or password?",
+                      style: GoogleFonts.montserrat(
+                        color: Colors.black54,
+                        fontWeight: FontWeight.w600,
+                        fontSize: 16,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: height * .03,
+                ),
+                Center(
+                  child: Text(
+                    "By proceding you also agree to the Terms of Service and Privacy Policy",
+                    textAlign: TextAlign.center,
+                    style: GoogleFonts.montserrat(
+                      color: Color(0xff2E303E).withOpacity(0.6),
+                      fontWeight: FontWeight.w400,
+                      fontSize: 14,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        )),
         //* fin de formulario
       ]),
     );
